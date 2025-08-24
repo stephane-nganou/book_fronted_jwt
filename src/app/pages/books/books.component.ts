f59e576c9b5174fc12a0773f00af834905b23f9a
StephaneWafo <stephanekechi@gmail.com>
2025-08-24 22:31:11 +0200

    add changes

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from "../menu/menu.component";
 
 @Component({
   selector: 'app-books',
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css'
 })
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,6 +7,21 @@ import { RouterLink } from '@angular/router';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  ngOnInit(): void {
+    const linkColor = document.querySelectorAll('.nav-link');
+    linkColor.forEach(link => {
+      if(window.location.href.endsWith(link.getAttribute('href') || '')){
+        link.classList.add('active');
+      }
+      link.addEventListener('click', () => {
+        linkColor.forEach(l => l.classList.remove('active'));
+        link.classList.add('active');
+      })
+    })
+  }
+  logout() {
+  throw new Error('Method not implemented.');
+}
 
 }
