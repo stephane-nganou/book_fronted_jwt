0e5c1bfa74eadb866a6b8f3f9e127fd5c8ee585d
StephaneWafo <stephanekechi@gmail.com>
2025-08-24 17:05:41 +0200

    same

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent, title: 'Login page'},
+    {path: '', component: MenuComponent, title: 'menu'},
     {path: 'activate-account', component: ActivateAccountComponent, title: 'Activattion page'},
     {path: 'login', component: LoginComponent, title: 'Login page'},
     {path: 'register', component: RegisterComponent, title: 'Register page'},
