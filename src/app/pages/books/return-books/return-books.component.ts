3789d0ab7b93deee9541a38c18b11449616571df
StephaneWafo <stephanekechi@gmail.com>
2025-08-31 18:18:52 +0200

    add return books components

---
+++ src/app/pages/books/return-books/return-books.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-return-books',
+  imports: [],
+  templateUrl: './return-books.component.html',
+  styleUrl: './return-books.component.css'
+})
+export class ReturnBooksComponent {
+
+}
