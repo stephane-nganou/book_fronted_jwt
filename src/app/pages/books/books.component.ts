0418bf267b4be8c888296d8e7233317c265e9da4
StephaneWafo <stephanekechi@gmail.com>
2025-09-07 15:35:25 +0200

    fixing BooksComponent

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -79,8 +79,12 @@ export class BooksComponent implements OnInit {
     this.findAllBooks();
   }
 
-  get isLastPage(): boolean {
-    return this.page === this.bookResponse?.total_pages as number - 1;
+  isLastPage(): boolean {
+    if (this.bookResponse === undefined || this.bookResponse === null) {
+      return true;
+    } else {
+      return this.page === this.bookResponse?.total_pages as number - 1;
+    }
   }
 
   private findAllBooks() {
